2edd4d78778c49466d9c3cdd125067c9120a2bbe
Nikhil <nikhlavida@gmail.com>
2012-10-09 01:02:08 +0530

    Initial commit

---
+++ Classes/ComDZJSONKitModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComDZJSONKitModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComDZJSONKitModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
